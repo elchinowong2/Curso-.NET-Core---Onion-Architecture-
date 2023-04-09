3044e9af40c86b573978cee78e502cb340ec9f50
elchinowong2 <thewong26@gmail.com>
2023-04-08 23:00:39 -0500

    a

---
+++ Persistencia/Context/applicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using aplication.Interfaces;
+using domain.Common;
+using domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia.Context
+{
+    public class applicationDbContext : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        private readonly iDateTimeServices _dateTime;
+        protected applicationDbContext(DbContextOptions<applicationDbContext> options, iDateTimeServices dateTime) 
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            _dateTime = dateTime;
+        }
+
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entity in ChangeTracker.Entries<AuditableBaseEntity>())
+            {
+                switch (entity.State)
+                {
+                    case EntityState.Added:
+                        entity.Entity.Created = _dateTime.NowUtc;
+                        break;
+                    case EntityState.Modified:
+                        entity.Entity.LastModified = _dateTime.NowUtc;
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ Persistencia/Repository/MyRepositoryAsync.cs
@@ -0,0 +1,21 @@
+﻿using Ardalis.Specification;
+using Ardalis.Specification.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Persistencia.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia.Repository
+{
+    public class MyRepositoryAsync<T> : RepositoryBase<T>, IRepositoryBase<T> where T : class
+    {
+        private readonly applicationDbContext _dbContext;
+        public MyRepositoryAsync(applicationDbContext dbContext) : base(dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+    }
+}
